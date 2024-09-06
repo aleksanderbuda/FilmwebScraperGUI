9e44981229c793530e9e1fa11d9541fdab2c2ded
abuda <abuda@solvd.com>
2024-09-06 13:34:01 +0200

    scraping logic improved

--- src/main/java/imdbscraper/model/SearchResultPage.java
+++ src/main/java/imdbscraper/model/SearchResultPage.java
@@ -1,6 +1,7 @@
 package imdbscraper.model;
 
 import lombok.Getter;
+import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,41 +12,43 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @Getter
 public class SearchResultPage {
+    private static final Logger LOGGER = Logger.getLogger(SearchResultPage.class.getName());
 
-    @FindBy(xpath = "//h3[@class=\"ipc-title__text\"]")
-    private WebElement title;
+    private static WebDriver driver;
+    private WebDriverWait wait;
 
-    @FindBy(className = "ipc-rating-star--rating")
-    private WebElement userScore;
-
-    @FindBy(className = "ipc-rating-star--voteCount")
-    private WebElement voteCount;
-
-    @FindBy(className = "ipc-html-content-inner-div")
-    private WebElement movieDescription;
-
-    @FindBy(xpath = "//span[contains(@class,'sc-b189961a-8 hCbzGp')][1]")
-    private WebElement releaseYear;
-
-    @FindBy(xpath = "//span[contains(@class,'sc-b189961a-8 hCbzGp')][2]")
-    private WebElement movieLength;
-
-    @FindBy(xpath = "//span[contains(@class,'sc-b189961a-8 hCbzGp')][3]")
-    private WebElement contentRating;
-
-//    @FindBy(xpath = "//ul[@class=\"ipc-metadata-list ipc-metadata-list--dividers-between sc-748571c8-0 jApQAb detailed-list-view ipc-metadata-list--base\"]")
-    @FindBy(xpath = "//li[@class=\"ipc-metadata-list-summary-item\"]")
-    private List<WebElement> singleMovieContainer;
+//    @FindBy(xpath = "//h3[@class=\"ipc-title__text\"]")
+//    private WebElement title;
+//
+//    @FindBy(className = "ipc-rating-star--rating")
+//    private WebElement userScore;
+//
+//    @FindBy(className = "ipc-rating-star--voteCount")
+//    private WebElement voteCount;
+//
+//    @FindBy(className = "ipc-html-content-inner-div")
+//    private WebElement movieDescription;
+//
+//    @FindBy(xpath = "//span[contains(@class,'sc-b189961a-8 hCbzGp')][1]")
+//    private WebElement releaseYear;
+//
+//    @FindBy(xpath = "//span[contains(@class,'sc-b189961a-8 hCbzGp')][2]")
+//    private WebElement movieLength;
+//
+//    @FindBy(xpath = "//span[contains(@class,'sc-b189961a-8 hCbzGp')][3]")
+//    private WebElement contentRating;
 
 //    @FindBy(className = "ipc-lockup-overlay ipc-focusable")
 //    private WebElement image;
 
-    private static WebDriver driver;
-    private WebDriverWait wait;
+@FindBy(xpath = "//div[@class='ipc-metadata-list-summary-item__tc']")
+private List<WebElement> movieContainers;
 
     public SearchResultPage(WebDriver driver) {
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
@@ -54,35 +57,58 @@ public class SearchResultPage {
 
     public List<Movie> getMovies() {
         System.out.println("Attempting to get movies...");
-        return singleMovieContainer.stream()
+        return movieContainers.stream()
                 .map(this::extractMovieData)
                 .distinct()
                 .collect(Collectors.toList());
     }
 
-    private Movie extractMovieData(WebElement resultElement) {
-        String title = resultElement.getText();
-        String userScore = resultElement.getText();
-        String voteCount = resultElement.getText();
-        String movieDescription = resultElement.getText();
-        String releaseYear = resultElement.getText();
-        String movieLength = resultElement.getText();
-        String contentRating = resultElement.getText();
+    private Movie extractMovieData(WebElement container) {
+        String titleText = container.findElement(By.xpath(".//h3[@class='ipc-title__text']")).getText();
+        titleText = titleText.substring(titleText.indexOf(' ') + 1);
 
-        return new Movie(title, userScore, voteCount, movieDescription, releaseYear, movieLength, contentRating);
+        String userScoreText = "----";
+        try {
+            userScoreText = container.findElement(By.className("ipc-rating-star--rating")).getText();
+        } catch (Exception e) {}
+
+        String voteCountText = "----";
+        try {
+            voteCountText = container.findElement(By.className("ipc-rating-star--voteCount")).getText();
+        } catch (Exception e) {}
+
+        String movieDescriptionText = "----";
+        try {
+            movieDescriptionText = container.findElement(By.className("ipc-html-content-inner-div")).getText();
+        } catch (Exception e) {}
+
+        String releaseYearText = "----";
+        try {
+            releaseYearText = container.findElement(By.xpath(".//span[contains(@class,'sc-b189961a-8 hCbzGp')][1]")).getText();
+        } catch (Exception e) {}
+
+        String movieLengthText = "----";
+        try {
+            movieLengthText = container.findElement(By.xpath(".//span[contains(@class,'sc-b189961a-8 hCbzGp')][2]")).getText();
+        } catch (Exception e) {}
+
+        String contentRatingText = "----";
+        try {
+            contentRatingText = container.findElement(By.xpath(".//span[contains(@class,'sc-b189961a-8 hCbzGp')][3]")).getText();
+        } catch (Exception e) {}
+
+        return new Movie(titleText, userScoreText, voteCountText, movieDescriptionText,
+                releaseYearText, movieLengthText, contentRatingText);
     }
 
     public void waitForSearchResults() {
         try {
-            System.out.println("Waiting for search results to load...");
-            wait.until(ExpectedConditions.visibilityOfAllElements(singleMovieContainer));
-            System.out.println("Movies container found. Found " + singleMovieContainer.size() + " container.");
+            LOGGER.log(Level.INFO, "Waiting for search results to load...");
+            wait.until(ExpectedConditions.visibilityOfAllElements(movieContainers));
+            LOGGER.log(Level.INFO,"Movies container found. Found " + movieContainers.size() + " containers.");
         } catch (TimeoutException e) {
-            System.out.println("Timeout waiting for search results to load.");
+            LOGGER.log(Level.WARNING, "Timeout waiting for search results to load.");
             throw e;
         }
     }
-
 }
-//iteruje teraz 7 razy każde scrapowane dane przez title, userscore itp. czyli np na title pokazuje wszystkie dane i  traktuje to jako 1. film znaleziony później 2 film to dla userscore ten sam a pozniej
-//dla votecount 3 raz i tak dalej, ma traktować każdy single container jako oddzielną rzecz i te 7 danych jest podawane dla jednego containera a nie inaczej.
